82c7b648b399f3d5ed4c831a8697cdb2653b5618
Araujopri <131909556+Araujopri@users.noreply.github.com>
2023-04-26 16:03:23 -0300

    Add files via upload

---
+++ cpf prova.py
@@ -0,0 +1,7 @@
+n = input("Digite o número do CPF: ")
+if n == "01732520198":
+    print ("MARIA")
+    if n == "01132453100":
+        print ("JOAO")
+        if n == "32423349512":
+            print ("CLAUDIO")
