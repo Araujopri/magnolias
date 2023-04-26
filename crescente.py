dde91df04496c1fc5b9b5546218158984cc797c5
Araujopri <131909556+Araujopri@users.noreply.github.com>
2023-04-26 16:04:48 -0300

    Add files via upload

---
+++ crescente.py
@@ -0,0 +1,7 @@
+n = int(input("Informe um número inteiro: "))
+o = int(input("Informe um número inteiro: "))
+p = int(input("Informe um número inteiro: "))
+if n < o and o < p:
+    print ("crescente")
+else:
+    print ("não está em ordem crescente")
